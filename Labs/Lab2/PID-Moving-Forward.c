025391aff786efa1905a7d1061f38390ca785998
boarrr <inomoncatnip@hotmail.com>
2024-01-31 10:49:36 +0000

    Push PID

---
+++ Labs/Lab2/PID-Moving-Forward.c
@@ -0,0 +1,1 @@
+/* Sample progams */void turnLeft(long time, long motorpower){		setMotorSpeed(motorB, -motorpower);	//Set the leftMotor (motor1) to half power (50)	setMotorSpeed(motorC, motorpower);  //Set the rightMotor (motor6) to half power (50)	sleep(time);						//Wait for 2 seconds before continuing on in the program.}task main(){	turnLeft(1000,50);	}
